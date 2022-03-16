16814c3510a28fef2608c5cf488e77f996453c68
Giadissima <52421987+Giadissima@users.noreply.github.com>
2022-03-16 15:13:24 +0100

    Update 15-03.ts

--- 15-03.ts
+++ 15-03.ts
@@ -1,1 +1,20 @@
-console.log("ciaoooo");
+// * DILEMMA DEL PRIGIONIERO
+enum Azioni{
+  Confessa,
+  NonConfessa
+}
+
+function prisonerDilemma(a1:Azioni, a2: Azioni): [Number, Number] {
+  // ? CASE: Confessa, Confessa
+  if(a1 == Azioni.Confessa && a2 == Azioni.Confessa)
+    return [-3, -3]
+  // ? CASE: Confessa, NonConfessa
+  if(a1 == Azioni.Confessa && a2 == Azioni.NonConfessa)
+    return [0,-6]
+  // ? CASE: NonConfessa, Confessa
+  if(a1 == Azioni.NonConfessa && a2 == Azioni.Confessa)
+    return [-6,0]
+  // ? CASE: NonConfessa, NonConfessa 
+  return [-1,-1]
+}
+console.log(prisonerDilemma(Azioni.NonConfessa, Azioni.NonConfessa))
