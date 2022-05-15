5a1333fdda409bca9be180ee06d911f4c234992b
Giadissima <52421987+Giadissima@users.noreply.github.com>
2022-05-15 10:00:22 +0200

    Update left_view.js

--- 11/5 alberi binari/left_view.js
+++ 11/5 alberi binari/left_view.js
@@ -5,21 +5,29 @@ T.val=7;
 T.sx={val: 4, sx: {val: 3}, dx: {val:-1, sx: {val: 4, dx:{val:3}, sx:{val: 8}}}}
 T.dx={val: 11, dx: {val: 1}, sx: {val:9, sx: {val: 20}}}
 
-//restituisce un array di nodi con aggiunto il lv
-function parse_tree(bt, h=0) {
-  if(bt == undefined || bt.val == undefined) return;
-  // caso foglia
-  if (bt.sx == undefined && bt.sx == undefined) return {val: bt.val, h: h}
-  let res_arr = []
+function stampa(T){
+  let arr_nodi = [];
+  let max = -Infinity;
 
-  let res_sx = parse_tree(bt.sx, h++);
-  if (res_sx !== undefined) res_arr.push(res_sx)
-  else{
-    let res_dx = parse_tree(bt.dx, h++);
-    if (res_dx !== undefined) res_arr.push(res_dx)
+  function parse_tree(node, h=0) {
+    // denota la profondità dell'albero
+    if(Math.max(max, h) == h) max = h;
+
+    if(node !== undefined || node.val !== undefined)
+      arr_nodi.push({val: node.val, h: h})
+    if(node.sx !== undefined) parse_tree(node.sx, h+1)
+    if(node.dx !== undefined) parse_tree(node.dx, h+1)
   }
-
-  return res_arr;
+  parse_tree(T);
+  //skippa i doppioni
+  for (let i= 0; i<=max; i++){
+    for(let j = 0; j< arr_nodi.length; j++){
+      if(arr_nodi[j].h == i) {
+        console.log(arr_nodi[j].val);
+        break;
+      }
+    }
+  }
 }
 
-console.log(parse_tree(T))
+stampa(T)
